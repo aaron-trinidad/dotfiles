36a5a364a3c44e6cf73a3c8d2369a56397dfaca2
Aaron Trinidad <aaron16trinidad02@gmail.com>
2026-05-17 10:19:37 -0600

    fix: spelllang now is defined globally and activate only for markdown files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.opt_local.spelllang = { "en_us", "es_mx" }
+
 -- Solo para markdown
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
     vim.opt_local.spell = true
-    vim.opt_local.spelllang = { "en", "es" }
   end,
 })
