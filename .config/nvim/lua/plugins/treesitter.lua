f92d8207342732a96294f603a9725ff83aeaa742
Aaron Trinidad <aaron16trinidad02@gmail.com>
2026-05-24 16:47:14 -0600

    updating treesitter API and branch

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+
+  config = function()
+    local ts = require("nvim-treesitter")
+
+    ts.setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    ts.install({
+      "lua",
+      "python",
+      "cpp",
+      "sql",
+      "bash",
+      "json",
+    }):wait(300000)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "lua",
+        "python",
+        "cpp",
+        "sh",
+        "json",
+      },
+      callback = function()
+        vim.treesitter.start()
+
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
