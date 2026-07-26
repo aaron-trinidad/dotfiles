cf13a0b410585281f4b31acd3f07095c615416a0
Aaron Trinidad <aaron16trinidad02@gmail.com>
2026-07-26 13:52:18 -0600

    update: including picom and new logic to the startup file

--- scripts/system/startup.sh
+++ scripts/system/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ------------------------------
+# MONITORES
+# ------------------------------
 ~/.config/scripts/monitors.sh || exit 1
 
 sleep 1
@@ -18,7 +21,12 @@ feh --bg-scale ~/Pictures/phm_wllpper.jpg
 # PICOM
 # ------------------------------
 killall picom 2>/dev/null
-picom >/dev/null 2>&1 &
+
+while pgrep -x picom >/dev/null; do
+  sleep 0.2
+done
+
+picom --config ~/.config/picom/picom.conf >/dev/null 2>&1 &
 
 # ------------------------------
 # POLYBAR
